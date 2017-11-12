09835eeea5a3256582c75e9ea9d7d386c7cf702b
tingwei628 <sbvq6p199xe@gmail.com>
2017-11-12 12:12:16 +0800

    delete files

--- src/utils/livereload.js
+++
@@ -1,9 +0,0 @@
-export default function livereload() {
-  if (process.env.NODE_ENV !== 'production') {
-    // Enable LiveReload
-    document.write(
-      '<script src="http://' + (location.host || 'localhost').split(':')[0] +
-      ':35729/livereload.js?snipver=1"></' + 'script>'
-    );
-  }
-}
