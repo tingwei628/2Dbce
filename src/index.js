95630cf2ae18e925c6a269de8743df90958b806c
tingwei628 <sbvq6p199xe@gmail.com>
2017-11-12 19:13:22 +0800

    add basic bounce motion

--- src/index.js
+++ src/index.js
@@ -1,4 +1,22 @@
-// import livereload from "./utils/livereload";
-// livereload();
-console.log("hello");
-document.getElementById("root").innerHTML = "Hello";
+import { GB, BB } from "./object";
+import "./style/canvas.css";
+document.getElementById("root").innerHTML = "gm5 - A ball collision game";
+const canvas = document.getElementById("gm5");
+const ctx = canvas.getContext("2d");
+
+let gb1 = new GB(ctx, 100, 50);
+let bb1 = new BB(ctx, 50, 100);
+
+//simple time
+let cancelId = null;
+
+function start() {
+  gb1.move(ctx);
+  bb1.move(ctx);
+  cancelId = requestAnimationFrame(start);
+}
+
+start();
+
+// when to end?
+// cancelAnimationFrame(cancelId);
--- src/object/index.js
+++ src/object/index.js
@@ -1,10 +1,8 @@
 // gb - good ball
-const vx = 2;
-const vy = 2;
 
-function create(ctx, x, y) {
+function create(ctx, x, y, r) {
   ctx.beginPath();
-  ctx.arc(x, y, 20, 0, Math.PI * 2, false);
+  ctx.arc(x, y, r, 0, Math.PI * 2, false);
   ctx.fillStyle = "green";
   ctx.fill();
   ctx.closePath();
@@ -13,18 +11,35 @@ function clear(ctx) {
   ctx.clearRect(0, 0, 500, 500);
 }
 
+function stepx(x, dx, r) {
+  if (x + dx > (500 - r) || (x + dx < r)) {
+    dx = -dx;
+  }
+  return dx;
+}
+function stepy(y, dy, r) {
+  if (y + dy > (500 - r) || (y + dy < r)) {
+    dy = -dy;
+  }
+  return dy;
+}
+
 class GB {
-  constructor(ctx, x = 240, y = 160) {
+  constructor(ctx, x = 240, y = 160, r = 10) {
     this.gb_x = x;
     this.gb_y = y;
-    create(ctx, this.gb_x, this.gb_y);
+    this.gb_r = r;
+    this.vx = 3;
+    this.vy = -3;
+    create(ctx, this.gb_x, this.gb_y, this.gb_r);
   }
-  move(ctx, dt) {
+  move(ctx) {
     clear(ctx);
-    create(ctx, this.gb_x, this.gb_y);
-    this.gb_x += vx * dt;
-    console.log("exe");
-    //this.gb_y += vy;
+    create(ctx, this.gb_x, this.gb_y, this.gb_r);
+    this.vx = stepx(this.gb_x, this.vx, this.gb_r);
+    this.vy = stepy(this.gb_y, this.vy, this.gb_r);
+    this.gb_x += this.vx;
+    this.gb_y += this.vy;
   }
 }
 
@@ -32,17 +47,22 @@ class GB {
 
 // bb - bad ball
 class BB {
-  constructor(ctx, x = 240, y = 160) {
+  constructor(ctx, x = 240, y = 160, r = 10) {
     this.bb_x = x;
     this.bb_y = y;
-    create(ctx, this.bb_x, this.bb_y);
+    this.bb_r = r;
+    this.vx = 3;
+    this.vy = -3;
+    create(ctx, this.bb_x, this.bb_y, this.bb_r);
   }
 
-  move(ctx, dt) {
+  move(ctx) {
     clear(ctx);
-    create(ctx, this.bb_x, this.bb_y);
-    this.bb_x += vx * dt;
-    this.bb_y += vy * dt;
+    create(ctx, this.bb_x, this.bb_y, this.bb_r);
+    this.vx = stepx(this.bb_x, this.vx, this.bb_r);
+    this.vy = stepy(this.bb_y, this.vy, this.bb_r);
+    this.bb_x += this.vx;
+    this.bb_y += this.vy;
   }
 }
 
