0e78e2d510e19a33febc393bf583520626a16659
tingwei628 <sbvq6p199xe@gmail.com>
2017-11-12 12:10:08 +0800

    rollup config setring

---
+++ rollup.config.js
@@ -0,0 +1,41 @@
+import babel from 'rollup-plugin-babel';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import replace from 'rollup-plugin-replace';
+import uglify from 'rollup-plugin-uglify';
+import postcss from 'rollup-plugin-postcss';
+import cssnano from 'cssnano';
+
+const isProd = process.env.NODE_ENV === 'production' ? true : false;
+
+export default {
+  input: './src/index.js',
+  output: {
+    file: 'build/js/index.min.js',
+    format: isProd ? 'umd' : 'iife',
+    sourcemap: isProd ? true : false
+  },
+  plugins: [
+    postcss({
+      plugins: [
+        cssnano()
+      ],
+      extensions: ['.css'],
+    }),
+    resolve({
+      jsnext: true,
+      main: true,
+      browser: true,
+    }),
+    commonjs(),
+    babel({
+      exclude: 'node_modules/**',
+    }),
+    replace({
+      exclude: 'node_modules/**',
+      ENV: JSON.stringify(process.env.NODE_ENV || 'development'),
+    }),
+    (process.env.NODE_ENV === 'production' && uglify()),
+  ],
+  external: ["express"]
+};
